9b52ba95c44f38bf91af2220b4c745e0b9858d32
tannerru <tanner.rusher@gmail.com>
2023-02-06 00:17:30 -0600

    Adding example for terragrunt structure

---
+++ #Example/Modules/ubuntu_2204/variables.tf
@@ -0,0 +1,31 @@
+variable "ssh_password" {
+  type        = string
+}
+
+variable "ssh_user" {
+  type        = string
+}
+
+variable "ssh_pub_key" {
+  type        = string
+}
+
+variable "k3s_masters" {
+  type        = map(any)
+}
+
+
+variable "k3s_source_template" {
+  type = string
+  
+}
+
+variable "ciuser" {
+  type = string
+  
+}
+
+variable "cipassword" {
+  type = string
+  
+}
